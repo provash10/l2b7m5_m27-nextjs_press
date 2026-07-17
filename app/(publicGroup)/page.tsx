2ac423decfa38bf4b1c66fe43f28fd97a2cd1e00
Provash <provash20cb@gmail.com>
2026-07-17 19:04:46 +0600

    28-6 Project Route Structuring Of Our First Frontend Project

---
+++ app/(publicGroup)/page.tsx
@@ -0,0 +1,9 @@
+
+export default function Home(){
+  console.log("Root Route")
+  return(
+    <div>
+      Hello Nextjs !! 
+    </div>
+  );
+}
--- app/ui/MyServerComponent.tsx
+++
@@ -1,7 +0,0 @@
-import React from "react";
-
-const MyServerComponent = () => {
-  return <div>MyServerComponent</div>;
-};
-
-export default MyServerComponent;
