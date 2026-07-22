df70d0fa5791cbef48ec9e8c6bd5faa42b688fd7
Provash <provash20cb@gmail.com>
2026-07-22 13:17:51 +0600

    30-5 Handling Auth Routes In Proxy File

--- proxy.ts
+++ proxy.ts
@@ -1,27 +1,41 @@
-import { NextResponse } from 'next/server'
-import type { NextRequest } from 'next/server'
- 
-// This function can be marked `async` if using `await` inside
-export function proxy(request: NextRequest) {
-    const pathname = request.nextUrl.pathname;
-    console.log(request,"request");
-    console.log(pathname,"pathname");
-    console.log("Proxy");
-//   return NextResponse.redirect(new URL('/', request.url))
-return NextResponse.next()
+import { NextResponse } from "next/server";
+import type { NextRequest } from "next/server";
+import jwt, { JwtPayload } from "jsonwebtoken";
+
+const AUTH_ROUTES = ["/login", "/register"];
+
+export async function proxy(request: NextRequest) {
+  const pathname = request.nextUrl.pathname;
+  const accessToken = request.cookies.get("accessToken")?.value ?? null;
+  const decodedToken = accessToken
+    ? (jwt.decode(accessToken) as JwtPayload | null)
+    : null;
+
+  let userRole: string | null = null;
+  if (
+    decodedToken &&
+    typeof decodedToken === "object" &&
+    "role" in decodedToken
+  ) {
+    userRole = decodedToken.role as string;
+  }
+
+  if (accessToken && AUTH_ROUTES.includes(pathname)) {
+    if (userRole === "USER") {
+      return NextResponse.redirect(new URL("/dashboard", request.url));
+    }
+    if (userRole === "ADMIN") {
+      return NextResponse.redirect(new URL("/admin-dashboard", request.url));
+    }
+    if (userRole === "AUTHOR") {
+      return NextResponse.redirect(new URL("/author-dashboard", request.url));
+    }
+    return NextResponse.redirect(new URL("/", request.url));
+  }
+
+  return NextResponse.next();
 }
- 
-// Alternatively, you can use a default export:
-// export default function proxy(request: NextRequest) { ... }
- 
+
 export const config = {
-//   matcher: '/dashboard/:path*',
-   matcher: [
-    // '/dashboard/:path*',
-    // '/admin-dashboard/:path*',
-    '/((?!api|_next/static|_next/image|.*\\.png$).*)',
-   ]
-
-//   matcher: ['/dashboard', '/dashboard/:path*']
-
-}
+  matcher: ["/((?!api|_next/static|_next/image|.*\\.png$).*)"],
+};
