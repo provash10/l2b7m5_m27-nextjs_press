d07406c512366f1c73c849643205a977c3cc4036
Provash <provash20cb@gmail.com>
2026-07-22 13:38:57 +0600

    30-6 Authenticating Protected Routes In Proxy

--- proxy.ts
+++ proxy.ts
@@ -3,6 +3,8 @@ import type { NextRequest } from "next/server";
 import jwt, { JwtPayload } from "jsonwebtoken";
 
 const AUTH_ROUTES = ["/login", "/register"];
+// const PUBLIC_ROUTES = ["/", "/news", "/login", "/register"];
+const PUBLIC_ROUTES = ["/", "/news"];
 
 export async function proxy(request: NextRequest) {
   const pathname = request.nextUrl.pathname;
@@ -31,6 +33,18 @@ export async function proxy(request: NextRequest) {
       return NextResponse.redirect(new URL("/author-dashboard", request.url));
     }
     return NextResponse.redirect(new URL("/", request.url));
+  }
+
+  const isPublicRoute = PUBLIC_ROUTES.some(
+    (route) => pathname === route || pathname.startsWith(route + "/")
+  );
+  const isAuthRoute = AUTH_ROUTES.some(
+    (route) => pathname === route || pathname.startsWith(route + "/")
+  );
+
+  //Authenticated Pages Protection : Authorization is not handle yet..
+  if (!accessToken && !isPublicRoute && !isAuthRoute) {
+    return NextResponse.redirect(new URL("/login", request.url));
   }
 
   return NextResponse.next();
